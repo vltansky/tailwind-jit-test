063ab45e9f2906a6d81afa03ec54c04002585e5d
Vlad <vl.tansky@gmail.com>
2021-04-06 23:10:12 +0300

    feat: jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-// const { guessProductionMode } = require("@vltansky/tailwind");
 process.env.TAILWIND_MODE = process.env.WEBPACK_DEV_SERVER === 'true' && process.argv.join(' ').indexOf('build') !== -1
     ? 'build'
     : 'watch';
+
 module.exports = {
     prefix: '',
     mode: 'jit',
     purge: {
-      // enabled: guessProductionMode(),
       content: [
         './src/**/*.{html,ts}',
       ]
